7ea4ed02f50365c7330db10d31565a80463978de
ramfox <klhuizinga@gmail.com>
2018-01-06 02:15:15 -0500

    chapter 3 exercises! :)

---
+++ ex03.13/main.go
@@ -0,0 +1,31 @@
+// ex 03.13 Write const declarations for KB-YB as compactly as you can
+//
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	KB = 1000
+	MB = 1000 * KB
+	GB = 1000 * MB
+	TB = 1000 * GB
+	PB = 1000 * TB
+	EB = 1000 * PB
+	ZB = 1000 * EB
+	YB = 1000 * ZB
+)
+
+func main() {
+	fmt.Println(KB)
+	fmt.Println(MB)
+	fmt.Println(GB)
+	fmt.Println(TB)
+	fmt.Println(PB)
+	fmt.Println(EB)
+	// fmt.Println(ZB)
+	// fmt.Println(YB)
+}
+
+//!-
